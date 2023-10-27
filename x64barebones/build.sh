e5eb69fdcf3317f2cedc92226206c0ee29ccc244
TomxBC <tobecerra@itba.edu.ar>
2023-10-27 16:34:57 -0300

    git ignore updated

--- x64barebones/build.sh
+++ x64barebones/build.sh
@@ -4,4 +4,5 @@ result=${PWD##*/}
 docker start $result
 docker exec -w /root/Toolchain -it $result make clean all
 docker exec -w /root/ -it $result make clean all
+docker exec -w /root/Image -it $result chmod 777 *
 docker stop $result
