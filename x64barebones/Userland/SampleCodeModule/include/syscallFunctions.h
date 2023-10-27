340ce2bf8b0c8e216e5031a862ef4a5b8b2a8ac5
mcullen212 <106096956+mcullen212@users.noreply.github.com>
2023-10-27 19:08:35 -0300

    Started implementing the c-library functions

--- x64barebones/Userland/SampleCodeModule/include/syscallFunctions.h
+++ x64barebones/Userland/SampleCodeModule/include/syscallFunctions.h
@@ -4,8 +4,8 @@
 #include <stddef.h>
 #include <stdint.h>
 
-int call_read(unsigned int fd, char *buf, size_t count);
-void call_write(unsigned int fd, const char *buf, size_t count);
+int call_read(char *buf, size_t count);
+void call_write(const char *buf, size_t count);
 void call_draw_char(uint8_t character, uint32_t hexColor, uint64_t x, uint64_t y, uint32_t scale);
 void call_delete_char(uint32_t hexColor, uint64_t x, uint64_t y, uint32_t scale);
 
