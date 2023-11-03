2db8c1ce9f417b036683f37a7898028d8bd6a7f2
mcullen212 <106096956+mcullen212@users.noreply.github.com>
2023-11-03 14:17:38 -0300

    manual corrected

--- x64barebones/Userland/SampleCodeModule/Commands/man.c
+++ x64barebones/Userland/SampleCodeModule/Commands/man.c
@@ -3,9 +3,10 @@
 void man(){
     printf("Welcome to Wuevo\n");
     printf("NAME\n");
-    printf("\t man - is an the manual to the system refernce\n\n");
+    printf("\t man - the manual to the system refernce\n\n");
     printf("DESCRIPTION\n");
-    printf("\tThis manual is a project for Computer Architecture created by Agustin Alonso, Magdalena Cullen, and Tomas Becerra.\n\n");
+    printf("\t This is an assignment for a Computer Architecture Course at ITBA.\n");
+    printf("\t Created by Agustin Alonso, Magdalena Cullen, and Tomas Becerra.\n\n");
     printf("PROGRAMS AVAILABLE FOR USER\n");
     printf("\t man - Access the manual.\n");
     printf("\t time - Retrieve the current time.\n");
